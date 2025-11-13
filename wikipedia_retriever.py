01600bedcc5d16f2ca714326cbcb964fdee7f618
Abhinav Kumar <abhinavkumarsaksena@gmail.com>
2025-11-13 23:09:17 +0530

    retrievers

--- wikipedia_retriever.py
+++
@@ -1,6 +0,0 @@
-from langchain_community.retrievers import WikipediaRetriever
-
-retriever = WikipediaRetriever(top_k_results=2, lang="en")
-query = "the geopolitical history of india and pakistan from the perspective of a chinese"
-
-docs = retriever.invoke(query)
